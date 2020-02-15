f3006a947e2bac5ebbc51f123aeb385e18dc3294
501 <501@academicos.cesde.edu.co>
2020-02-14 21:01:12 -0500

    Esta es la version funcional de mi codigo

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+//Esto es un comentario de linea
+
+/*Este es un comentario 
+de varias lineas*/
+
+$nombre = "Daniel Stiven";
+echo($nombre);
+$edad = 20;
+echo($edad);
+$estatura = 1.50;
+$nombre = "Angela";
+echo($edad);
+echo($nombre);
+
+//$edad = 105;
+echo($edad);
+
+echo("Hola mundo");
+print("<br>");
+echo("Mi nombre es ".$nombre."y la edad es: ".$edad);
+
+
+
+
+
+
+?>
